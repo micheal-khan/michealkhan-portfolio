65657bde2ef5a282c6e897248b5d228b7bdbc049
Micheal <khanzaidan786@gmail.com>
2025-08-26 15:33:14 +0530

    Speed Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const queryClient = new QueryClient();
 
@@ -21,6 +22,7 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
         <Analytics/>
+        <SpeedInsights/>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
